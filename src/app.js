0d81bc99ce8aacf21ef64e1b620689c311bbb17c
blorein-code <bblorein@gmail.com>
2021-10-19 00:46:36 +0300

    18ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log("Merhaba Kodlama.io")
+
+
+
+let konutKredileri = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
